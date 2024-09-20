e454c752c2b3e813f287a5f2a0f9757e0e24c482
Joy Leila Kendi <muriungikendileila@gmail.com>
2024-09-20 14:54:20 +0300

    Code review

--- controllers/image.js
+++ controllers/image.js
@@ -43,7 +43,6 @@ const handleApiCall = (req, res) => {
   .catch(err => res.status(400).json('unable to work with API'));
 }
 
-
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
